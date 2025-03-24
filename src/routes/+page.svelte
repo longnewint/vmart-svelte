dc804ed2042f3c66aec2491e472db525978cb77f
Long Nguyen <longnewint@gmail.com>
2025-03-23 22:36:28 -0400

    Modify route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script>
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+
+    const resp = await fetch('http://localhost:8080/product/c/10?storeId=101');
+    console.log(resp.json());
+  });
+
+</script>
