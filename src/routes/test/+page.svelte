10d96deee9e582e04e1fb73f626b46730f7d0e22
Long Nguyen <longnewint@gmail.com>
2025-03-23 22:37:01 -0400

    Add test route

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    
+    const resp = await fetch('http://localhost:8080/product/c/9?storeId=101');
+    console.log(resp.json());
+  });
+</script>
