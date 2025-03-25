be4f2231aeccb4893db19700f0c646f7e3544755
Long Nguyen <longnewint@gmail.com>
2025-03-25 19:14:31 -0400

    Add helpers file

---
+++ src/helpers/storage.ts
@@ -0,0 +1,20 @@
+export const getStoreArray = (store: string) => {
+  return localStorage.getItem(store)
+    ? localStorage.getItem(store)?.split(', ')
+    : [];
+};
+
+export const updateStore = (store: string, items: string[], item?: string) => {
+  if (!item) {
+    localStorage.setItem(store, items.join(', '));
+  } else {
+    localStorage.setItem(
+      store,
+      items.join(', ').concat(items.length ? `, ${item}` : `${item}`)
+    );
+  }
+};
+
+export const removeStore = (store: string) => {
+  localStorage.removeItem(store);
+};
