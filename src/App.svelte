ba9ac529c6d8bf23c317bc120aa4e7956ff8876a
Long Nguyen <longnewint@gmail.com>
2025-03-26 22:29:27 -0400

    Add component to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-  import Footer from "./lib/footer/Footer.svelte";
   import '@fortawesome/fontawesome-free/css/all.min.css';
+  import Header from './lib/Organisms/Header.svelte';
+  import Index from './lib/Template/Index.svelte';
+    import Footer from './lib/Organisms/Footer.svelte';
+  
+  let innerWidth: number = 0;
 </script>
 
+<svelte:window bind:innerWidth />
+
+<Header {innerWidth} />
+<Index />
 <Footer />
