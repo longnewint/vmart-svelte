a1913bebd652eaeb6da63934ac159edb1fc0d821
Long Nguyen <longnewint@gmail.com>
2025-03-26 22:28:27 -0400

    Add template component

---
+++ src/lib/Template/Index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Newsletter from '../Organisms/Newsletter.svelte';
+  import Products from '../Organisms/Products.svelte';
+  import Hero from '../Organisms/Hero.svelte';
+  import Faqs from '../Organisms/Faqs.svelte';
+</script>
+
+<main>
+  <Hero />
+  <Products />
+  <Newsletter />
+  <Faqs />
+</main>
