421ba18ea478d40393df7b35f264796c7d0ea448
Long Nguyen <longnewint@gmail.com>
2025-03-30 18:19:01 -0400

    Modify App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import '@fortawesome/fontawesome-free/css/all.min.css';
-  import Header from './lib/Organisms/Header.svelte';
-  import Index from './lib/Template/Index.svelte';
-  import Footer from './lib/Organisms/Footer.svelte';
+  import Header from './components/Header.svelte';
+  import Main from './components/Main.svelte';
+  import Footer from './components/Footer.svelte';
   
   let innerWidth: number = 0;
 </script>
@@ -10,5 +10,5 @@
 <svelte:window bind:innerWidth />
 
 <Header {innerWidth} />
-<Index />
+<Main />
 <Footer />
