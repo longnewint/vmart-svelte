e55a3b39a7a49fe728386f3da0eca916ea915fc0
Long Nguyen <longnewint@gmail.com>
2025-03-30 18:17:51 -0400

    rename Index to Main and move file to src/components

---
+++ src/components/Main.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Newsletter from './main/newsletter/Newsletter.svelte';
+  import Products from './product/Products.svelte';
+  import Hero from './main/hero/Hero.svelte';
+  import Faqs from './main/faqs/Faqs.svelte';
+</script>
+
+<main>
+  <Hero />
+  <Products />
+  <Newsletter />
+  <Faqs />
+</main>
